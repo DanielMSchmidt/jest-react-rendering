c87f6b54503250cecc622918e89710bcf3f7dc1e
Daniel Schmidt <dschmidt@mesosphere.com>
2019-04-12 12:24:47 +0200

    chore: add first tests

---
+++ packages/tests-for-jest-environment/__tests__/index-test.js
@@ -0,0 +1,22 @@
+/**
+ * @jest-environment react-rendering
+ */
+
+import React from "react";
+import TestRenderer from "react-test-renderer";
+
+function MyComponent() {
+  return <h1>Hello World</h1>;
+}
+
+describe("ReactRendering", () => {
+  it("behaves like JSDOM", () => {
+    const renderer = TestRenderer.create(<MyComponent />);
+
+    expect(renderer.toJSON()).toMatchInlineSnapshot(`
+      <h1>
+        Hello World
+      </h1>
+    `);
+  });
+});
