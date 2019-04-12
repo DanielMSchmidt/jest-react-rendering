9dc251e4d3947643362f0c89b930e49cee735eb7
Daniel Schmidt <dschmidt@mesosphere.com>
2019-04-12 17:33:11 +0200

    chore: improve setup

--- packages/jest-react-rendering/src/index.js
+++ packages/jest-react-rendering/src/index.js
@@ -1,23 +1,18 @@
-function setup(expect = global.expect) {
-  if (!expect) {
-    console.error(
-      "Coud not setup jest-react-rerendering, no global expect known and no specific one passed"
-    );
-  } else {
-    expect.extend({
-      toBeUpdatedTimes(Component, expectedTimes) {
-        const renderingMap = global.__react_rerenders || {};
-        const actualTimes = renderingMap[Component] || 0;
-        const pass = actualTimes === expectedTimes;
+if (!global.expect) {
+  console.error(
+    "Coud not setup jest-react-rerendering, no global expect known and no specific one passed"
+  );
+} else {
+  global.expect.extend({
+    toBeUpdatedTimes(Component, expectedTimes) {
+      const renderingMap = global.__react_rerenders || {};
+      const actualTimes = renderingMap[Component] || 0;
+      const pass = actualTimes === expectedTimes;
 
-        return {
-          message: () =>
-            `expected ${expectedTimes} renders, got ${actualTimes}`,
-          pass
-        };
-      }
-    });
-  }
+      return {
+        message: () => `expected ${expectedTimes} renders, got ${actualTimes}`,
+        pass
+      };
+    }
+  });
 }
-
-module.export = setup;
